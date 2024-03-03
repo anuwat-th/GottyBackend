716ce3062284eff3a63921b2e0f5f6c4b8b54699
“anuwat-thongkham” <anuwat.one@icloud.com>
2024-03-03 20:39:57 +0700

    Test Server

--- server.js
+++ server.js
@@ -1,11 +1,35 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+const User = require('./model/User');
+
+mongoose.set('strictQuery', false);
+const connectDB = async () => {
+    try {
+        const connect = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB is Connected: ${connect.connection.host}`);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+}
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    // res.send('Hello World');\
+    res.send({title: 'User'});
 });
 
-const HTTP_PORT = process.env.PORT || 8080;
-app.listen(HTTP_PORT, () => {
-    console.log(`Server is listening at port ${HTTP_PORT}`);
+// app.listen(PORT, () => {
+//     console.log(`Server is listening at port ${PORT}`);
+// });
+
+connectDB().then(()=>{
+    app.listen(PORT, ()=>{
+        console.log(`Listening on port ${PORT}`);
+    })
 });
