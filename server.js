074c21ec7c4b8687e0cb0d6405988241e38f1ed9
“anuwat-thongkham” <anuwat.one@icloud.com>
2024-03-03 15:05:52 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,3 +4,8 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
+
+const HTTP_PORT = process.env.PORT || 8080;
+app.listen(HTTP_PORT, () => {
+    console.log(`Server is listening at port ${HTPP_PORT}`);
+});
