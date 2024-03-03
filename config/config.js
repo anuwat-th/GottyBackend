a54c61a29712e094060a0ee2471cda451759e801
“anuwat-thongkham” <anuwat.one@icloud.com>
2024-03-03 21:09:14 +0700

    test server config

---
+++ config/config.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+mongoose.set('strictQuery', false);
+
+const connectDB = async () => {
+    try {
+        const connect = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB is Connected: ${connect.connection.host}`);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+}
+
+connectDB().then(()=>{
+    app.listen(PORT, ()=>{
+        console.log(`Listening on port ${PORT}`);
+    })
+});
+
+module.exports = { app, PORT };
--- server.js
+++ server.js
@@ -1,35 +1,18 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-
-const app = express();
-const PORT = process.env.PORT || 5000;
-
+const { app, PORT } = require('./config/config');
 
 const User = require('./model/User');
 
 mongoose.set('strictQuery', false);
-const connectDB = async () => {
-    try {
-        const connect = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB is Connected: ${connect.connection.host}`);
-    } catch (error) {
-        console.log(error);
-        process.exit(1);
-    }
-}
 
 app.get('/', (req, res) => {
     // res.send('Hello World');\
-    res.send({title: 'User'});
+    res.send({ title: 'User' });
 });
 
 // app.listen(PORT, () => {
 //     console.log(`Server is listening at port ${PORT}`);
 // });
 
-connectDB().then(()=>{
-    app.listen(PORT, ()=>{
-        console.log(`Listening on port ${PORT}`);
-    })
-});
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+})
