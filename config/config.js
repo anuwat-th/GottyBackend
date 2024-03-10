57174eeb7b86a548b6610156cc68054434016967
“anuwat-thongkham” <anuwat.one@icloud.com>
2024-03-10 22:33:13 +0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const { MongoClient } = require('mongodb');
+const MongoClient = require('mongodb');
 const bodyParser = require('body-parser');
 
 const app = express();
