a1fa7aee9d4e9205812666f2d9e5361b47269c92
“anuwat-thongkham” <anuwat.one@icloud.com>
2024-03-11 21:22:25 +0700

    Update SendEmailOTP.js

--- helper/SendEmailOTP.js
+++ helper/SendEmailOTP.js
@@ -23,9 +23,24 @@ const transporter = nodemailer.createTransport({
 const htmlForm = (otpCode) => {
     return (
         `
-            <div style="margin:0;bacground:#FFFFFF;justify-content:center;align-items:center;">
-                <h1 style="font-size:30px;color:#C81D25;">${otpCode}</h1> 
-            </div>
+        <div
+        style="width: 1024px;height: 1024px;margin: 0; background-color: #C81D25; display: flex; flex-direction: column; border: 1px solid black;">
+        <img src="https://orange-ant-kit.cyclic.app/gotty-logo" alt="Gotty logo"
+            style="width: 224px; height: 224px; margin: 32px auto 0 auto;">
+        <div
+            style="width: 1024px; height: 752px;background-color: #FFFFFF; margin: 16px 0 0 0; border-radius: 64px 64px 0 0;">
+            <h1
+                style="width: 1024px;height: 80px; margin: 48px 0 0 0; color: #C81D25; font: 700 48px Arial, serif;display: flex;align-items: center;justify-content: center;">
+                Wellcom to Gotty</h1>
+            <p
+                style="width: 1024px;height: 144px;margin: 16px 0 0 0;  color: #333335; font: 400 24px Arial;justify-content: center; display: flex;text-align: center;">
+                This is your OTP code for Sign up to Gotty, <br>Please do not tell this code to another.
+                the code can available for only 3 minutes.</p>
+            <h2
+                style="width: 464px; height: 176px;margin: 0 auto; border-radius: 40px; display: flex;justify-content: center;align-items: center; box-shadow: rgba(255, 90, 95, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;  font: 700 48px Arial, serif; color: #C81D25;">
+                ${otpCode}</h2>
+        </div>
+    </div>
         `
     );
 }
