e0706b0feae70b99266e697e0e8273af21161d45
“anuwat-thongkham” <anuwat.one@icloud.com>
2024-03-11 21:33:47 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,13 +3,34 @@ const { app, connectDB, PORT } = require('./config/config');
 const GetLogoImage = require('./controller/GetLogoImage');
 const SignUpWithEmailStep1 = require('./controller/SignUpWithEmailStep1');
 
+const User = require('./model/User')
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 })
 
 connectDB();
+const mockData = async (req, res) => {
+    try {
+        const userDate = new User({
+            id: 'asdfghjklpoiuyt',
+            handle: 'user1_handle',
+            email: 'user1@example.com',
+            authType: 'google_gmail',
+            password: 'hashed_password_1',
+            username: 'user1_username',
+            createdAt: new Date('2023-01-01'),
+            updatedAt: new Date('2023-01-03')
+        });
+        await userDate.save();
+        res.status(200).send('OTP sent successfully');
+    } catch (error) {
+        console.error('Error saving SignUpWithEmailSession:', error);
+        res.sendStatus(500);
+    }
+}
 
+app.post('/inputFake', mockData);
 app.get('/gotty-logo', GetLogoImage);
 
 app.get('/', (req, res) => {
@@ -17,4 +38,5 @@ app.get('/', (req, res) => {
 });
 
 app.post('/sign-up-with-email-step-1', SignUpWithEmailStep1);
+
 
